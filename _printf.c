946c3f41be561b47c6d1d69abb1b3ad4ba9429fe
Tatenda35 <tatndor@gmail.com>
2023-11-14 11:13:18 -0800

    Revised main.h
    Added string.

---
+++ _printf.c
@@ -0,0 +1,68 @@
+#include "main.h"
+#include <stdarg.h>
+
+
+int _printf(const char *format, ...)
+{
+	int charactersthatwewillprint = 0;
+
+
+
+
+	va_list listofourarguments;
+	
+	if(format == NULL)
+	{
+		return(-1);
+	}
+
+
+
+	va_start (listofourarguments,format);
+
+
+	while(*format)
+	
+	{
+		if(*format != '%')
+		{
+			write(1, format, 1);
+			charactersthatwewillprint++;
+		}
+		else
+		{
+			format++;
+		}
+		if(format == '\0')
+		{
+			break;
+
+		}
+		if(format == 'c')
+		{
+			int num = va_arg(listofourarguments, int);
+			write(1 , num, strlen(num));
+		
+		}
+		if(format == 's')
+		{
+			strlen()
+
+		}
+		if(format == '%')
+		{
+
+		}
+
+	}
+
+
+
+
+
+	va_arg;
+
+
+
+	va_end;
+
